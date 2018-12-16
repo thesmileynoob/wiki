703f0d25136a903709e8ada3732c75f3418ae18b
realloc <realloc@protonmail.com>
2018-12-16 11:48:07 +0530

    add '/generate' for dummy data

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+import os
+
 import wasabi
 import flask
 import jinja2
 
 from wiki import config
 from wiki import page
+from wiki.page import Page, Revision
 
 log = wasabi.Printer()
 flask_app = flask.Flask('Wiki')
@@ -30,6 +33,46 @@ def todo():
 def settings():
     template = jenv.get_template('settings.html')
     return template.render()
+
+@flask_app.route('/generate')
+def generate_pages():
+    """ Generate dummy pages for testing """
+
+    if os.path.exists(config.DBNAME):
+        return f"Pages already present. Delete {config.DBNAME} to regenerate pages."
+
+    with open('static/dummy_data.txt', 'r') as f:
+        data = f.read()
+    
+    pages = data.split('======')
+    assert len(pages) == 3
+
+    p1 = Page(title='Wiki')
+    r1 = Revision(content=pages[0])
+
+    p2 = Page(title='Website')
+    r2 = Revision(content=pages[1])
+
+    p3 = Page(title='Stock_Market')
+    r3 = Revision(content=pages[2])
+
+    session = page.new_session()
+
+    session.add(p1)
+    session.add(p2)
+    session.add(p3)
+    session.commit()
+
+    p1.revisions.append(r1)
+    p2.revisions.append(r2)
+    p3.revisions.append(r3)
+
+    session.commit()
+
+    session.close()
+
+    log.good('Pages generated successfully')
+    return 'OK'
 
 
 flask_app.run()
