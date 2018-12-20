176bb3b43a4d63911951e7ef3b4950a9089f660d
realloc <realloc@protonmail.com>
2018-12-20 18:53:12 +0530

    implement add page (NOT debugged)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import time
 
 import wasabi
 import flask
@@ -9,6 +10,7 @@ from wiki.page import Page, Revision, new_session
 
 log = wasabi.Printer()
 flask_app = flask.Flask('Wiki')
+flask_app.config['DEBUG'] = True
 
 
 @flask_app.route('/')
@@ -26,13 +28,16 @@ def homepage_view():
 @flask_app.route('/wiki/<title>')
 def page_view(title):
     session = new_session()
+
+    title = Page.format_title(title)
     page = session.query(Page).filter_by(title=title).one()
     if not page:
         return 'No match found'
     rev = page.revisions[0]
+
     session.close()
 
-    return flask.render_template('page.html', title=page.title,
+    return flask.render_template('page.html', title=Page.pretty_title(page.title),
                                  content=rev.content)
 
 
@@ -46,12 +51,60 @@ def settings():
     return flask.render_template('settings.html')
 
 
+@flask_app.route('/add', methods=['GET', 'POST'])
+def add_page_view():
+    """ Add a new page """
+    r = flask.request
+    if r.method == 'GET':
+        # Display the add form
+        return flask.render_template('add_page.html', title='Add Page')
+    else:
+        # Save the form and show a little modal maybe
+        page_title = r.form.get('page_title', '').strip()
+        page_flags = r.form.get('page_flags', '').strip()
+        page_note = r.form.get('page_note', '').strip()
+
+        rev_content = r.form.get('rev_content', '').strip()
+        rev_note = r.form.get('rev_note', '').strip()
+
+        if not page_title:
+            raise Exception('TitleError: empty title')
+        if not rev_content:
+            raise Exception('RevisionError: empty revision')
+
+        # page
+        page = Page()
+        page.title = Page.format_title(page_title)
+        page.note = page_note
+        page.flags = Page.format_flags(page_flags)
+
+        # Revision
+        rev = Revision()
+        rev.content = rev_content
+        rev.note = rev_note
+        rev.timestamp = int(time.time())
+
+        create_new_page(page, rev)
+
+        return flask.redirect(flask.url_for('page_view',
+                                            title=Page.format_title(page_title)))
+
+
+def create_new_page(page: Page, revision: Revision):
+    """ save's a new page to db """
+    session = new_session()
+    if page.revisions:
+        log.fail('create_new_page: Page already exists!: ', page)
+        raise Exception('Page already exists!')
+    page.revisions.append(revision)
+    session.add(page)
+    session.commit()
+    session.close()
+
+
 @flask_app.route('/generate')
 def generate_pages():
     """ Generate dummy pages for testing """
-
-    if os.path.exists(config.DBNAME):
-        return f"Pages already present. Delete {config.DBNAME} to regenerate pages."
 
     with open('static/dummy_data.txt', 'r') as f:
         data = f.read()
@@ -59,32 +112,22 @@ def generate_pages():
     pages = data.split('======')
     assert len(pages) == 3
 
-    p1 = Page(title='Wiki')
-    r1 = Revision(content=pages[0])
+    p1 = Page(title=Page.format_title('wiki'))
+    p1.revisions.append(Revision(content=pages[0]))
 
-    p2 = Page(title='Website')
-    r2 = Revision(content=pages[1])
+    p2 = Page(title=Page.format_title('Website'))
+    p2.revisions.append(Revision(content=pages[1]))
 
-    p3 = Page(title='Stock_Market')
-    r3 = Revision(content=pages[2])
+    p3 = Page(title=Page.format_title('stock market'))
+    p3.revisions.append(Revision(content=pages[2]))
 
     session = new_session()
-
-    session.add(p1)
-    session.add(p2)
-    session.add(p3)
+    session.add_all([p1, p2, p3])
     session.commit()
-
-    p1.revisions.append(r1)
-    p2.revisions.append(r2)
-    p3.revisions.append(r3)
-
-    session.commit()
-
     session.close()
 
     log.good('Pages generated successfully')
-    return 'OK'
+    return flask.redirect(flask.url_for('homepage_view'))
 
 
 flask_app.run()
--- wiki/page.py
+++ wiki/page.py
@@ -11,17 +11,9 @@ Represents a wiki page:
 Page:
     id: int
     title: str
-    categories: [int]
     flags: str
     notes: str
     body: Body
-
-Category:
-    id: int
-    name: str
-    desc: str
-    notes: str
-
 
 Revision:
     id: int
@@ -39,9 +31,8 @@ class Page(Base):
 
     id: int = Column(Integer, primary_key=True)
     title: str = Column(String, unique=True)
-    flags: str = Column(String, comment='Comma separated strings')
-    categories: int = Column(String, comment='Comma seperated category ids')
-    notes: str = Column(String)
+    flags: str = Column(String, comment='csv')
+    note: str = Column(String)
 
     # List of revisions of this page
     revisions = relationship('Revision', back_populates='page')
@@ -49,17 +40,27 @@ class Page(Base):
     def __repr__(self):
         return f"<Page(id: {self.id}, title: '{self.title}')>"
 
+    @staticmethod
+    def format_title(title: str):
+        """ returns string formatted title as stored in the db """
+        title = '_'.join(title.lower().split())
+        return title
 
-class Category(Base):
-    __tablename__ = 'categories'
+    @staticmethod
+    def pretty_title(title: str) -> str:
+        """ format page title in TitleCase """
+        thetitle = ' '.join(title.split('_'))  # the sunken ship
+        return thetitle.title()  # The Sunken Ship
 
-    id: int = Column(Integer, primary_key=True)
-    name: str = Column(String, unique=True)
-    desc: str = Column(String, comment='A short description of the category')
-    notes: str = Column(String)
-
-    def __repr__(self):
-        return f"<Category(id: {self.id}, name: '{self.name}')>"
+    @staticmethod
+    def format_flags(flags: any) -> str:
+        """ flags must be string or [string] """
+        if type(flags) not in [str, list]:
+            raise TypeError("flags must be str or [str]")
+        if type(flags) is str:
+            flags = flags.split(',')
+        flags = [x.strip().lower() for x in flags]
+        return ', '.join(flags)
 
 
 class Revision(Base):
@@ -69,12 +70,12 @@ class Revision(Base):
     page_id: int = Column(Integer, ForeignKey('pages.id'))
     content: str = Column(String)
     timestamp: int = Column(Integer)  # Unix epoch
-    notes: str = Column(String)
+    note: str = Column(String)
 
     page: Page = relationship('Page', back_populates='revisions')
 
     def __repr__(self):
-        return f"< Rev(id: {self.id}, pid: {self.page_id}, content: '{self.content}') >"
+        return f"<Rev(id: {self.id}, pid: {self.page_id}, content: '{self.content[:50]}')>"
 
 
 # Create the tables if they do not exist already
